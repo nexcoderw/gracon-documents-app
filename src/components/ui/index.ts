11121adaf1a7ede4df729f612b86e1a2bdecad6f
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-08-13 18:44:41 +0200

    chore(ui): export shared component types

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,5 +1,9 @@
 export { Button } from './Button';
+export type { ButtonProps, ButtonSize, ButtonVariant } from './Button';
 export { Input } from './Input';
+export type { InputProps } from './Input';
 export { Card } from './Card';
+export type { CardProps } from './Card';
 export { AppToaster, toast } from './Toast';
 export { PremiumLoader } from './Loader';
+export type { PremiumLoaderProps } from './Loader';
